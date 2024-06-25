aac5765b42cf06e3b712952f7db087d13f5072d5
idmaksim <i@dmaksim.ru>
2024-06-25 17:38:51 +0300

    add video

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import cv2
+
 from utils import get_input_filename
 from detection import track_cars, track_license_plates
 
@@ -15,8 +17,17 @@ def main() -> None:
 
         # TODO: save data(image with detected license and text, and csv file)
     elif ext in ['.mp4']: 
-        # TODO: do the same but with video and output video with detected plates and text
-        pass
+        cap = cv2.VideoCapture('./sample.mp4')
+        frame_num = -1
+        is_running = True
+        while is_running:
+            frame_num += 1
+            is_running, frame = cap.read()
+            car_coords = track_cars(filename)
+            license_plates_info = track_license_plates(filename, car_coords)
+
+        
+
     else:
         raise Exception('Unsupported file format')
     
